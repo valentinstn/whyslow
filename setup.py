3e4af8d26a5cd8a1fc6af71c8e517487d33a34ce
Valentin <valentin.stn@gmail.com>
2023-10-11 22:35:24 +0200

    add description

--- setup.py
+++ setup.py
@@ -1,13 +1,21 @@
 from setuptools import setup
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup( 
     name='whyslow', 
-    version='0.0.2', 
+    version='0.1', 
     description='Developer-friendly performance profiling utilities for Python', 
+    long_description=long_description,
+    long_description_content_type="text/markdown",    
     author='valentinstn', 
     packages=['whyslow'], 
     python_requires='>=3.6',
     install_requires=[ 
         'snakeviz'
     ], 
+    project_urls={
+        "GitHub": "https://github.com/valentinstn/whyslow",
+    },
 ) 
